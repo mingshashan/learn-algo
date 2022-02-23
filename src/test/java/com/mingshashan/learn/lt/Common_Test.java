214a2d8b6c96d593b5149d894811b94dd3adcf7d
xufangjie <xufangjie2008@163.com>
2022-02-23 08:32:22 +0800

    update

---
+++ src/test/java/com/mingshashan/learn/lt/Common_Test.java
@@ -0,0 +1,15 @@
+package com.mingshashan.learn.lt;
+
+import org.junit.Test;
+
+public class Common_Test {
+
+    @Test
+    public void test01() {
+        int x = 0xFFF;
+        System.out.printf("10: [%x] , 16: [%s]\n", x, Integer.toBinaryString(x));
+        int y = ~0xFFF;
+        System.out.printf("10: [%x] , 16: [%s]\n", y, Integer.toBinaryString(y));
+
+    }
+}

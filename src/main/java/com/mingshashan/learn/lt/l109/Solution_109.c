caf255d081461bf14a5cacc972c44ab3152bf1be
mingshashan <mingshashan@gmail.com>
2022-04-18 18:01:26 +0800

    daily practise

--- src/main/java/com/mingshashan/learn/lt/l109/Solution_109.c
+++ src/main/java/com/mingshashan/learn/lt/l109/Solution_109.c
@@ -26,6 +26,30 @@ struct TreeNode {
     struct TreeNode *left;
     struct TreeNode *right;
 };
-struct TreeNode* sortedListToBST(struct ListNode* head){
 
+struct ListNode *findMid(struct ListNode *head, struct ListNode *tail) {
+    struct ListNode *fast = head, *slow = head;
+    while (fast != tail && fast->next != tail) {
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+    
+    return slow;
+}
+
+struct TreeNode* toBST(struct ListNode* head, struct ListNode* tail){
+    if (head == NULL || head == tail) {
+        return NULL;
+    }
+
+    struct ListNode *mid = findMid(head, tail);
+    struct TreeNode *root = malloc(sizeof(struct TreeNode));
+    root->left = toBST(head, mid);
+    root->right = toBST(mid->next, tail);
+
+    return root;
+}
+
+struct TreeNode* sortedListToBST(struct ListNode* head){
+    return toBST(head, NULL);
 }

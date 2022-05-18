c26a5d964db2e33dcd43a653c3f4459a35a0b5e8
mingshashan <mingshashan@gmail.com>
2022-05-18 15:55:02 +0800

    daily practice

---
+++ src/main/java/com/mingshashan/learn/lt/l637/Solution.java
@@ -0,0 +1,33 @@
+package com.mingshashan.learn.lt.l637;
+
+import com.mingshashan.learn.lt.common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> result = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            double sum = 0;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (null != node.left) {
+                    queue.add(node.left);
+                }
+                if (null != node.right) {
+                    queue.add(node.right);
+                }
+                sum += node.val;
+            }
+
+            result.add(1.0 * sum / size);
+        }
+        return result;
+    }
+}
